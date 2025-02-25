a2388f6cf3c368aadb0c2fe9a323cac5e58188fa
Francogarcia02 <francoalbertogarcia2017@gmail.com>
2025-02-25 18:30:50 -0300

    New accepted origins

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -7,6 +7,9 @@ const ACCEPTED_ORIGINS = [
     'https://trainner-page-client.vercel.app',
     'https://trainner-page-client-francogarcia02-admins-projects.vercel.app',
     'https://trainner-page-client-git-main-francogarcia02-admins-projects.vercel.app',
+    'https://manualbarracin-planes-manualbarracins-projects.vercel.app/',
+    'https://manualbarracin-planes-git-main-manualbarracins-projects.vercel.app/',
+    'https://manualbarracin-planes.vercel.app/',
     /\.vercel\.app$/,
 ];
 
